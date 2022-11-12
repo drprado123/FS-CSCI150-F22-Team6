f029342d0cb8b1e28a11eb0671f57eee7807ef2b
RenGino <94270540+RenGino@users.noreply.github.com>
2022-11-11 19:32:51 -0800

    Update MainActivity.kt

--- app/src/main/java/com/example/omegacalendar/MainActivity.kt
+++ app/src/main/java/com/example/omegacalendar/MainActivity.kt
@@ -1,43 +1,39 @@
 package com.example.omegacalendar
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.omegacalendar.ui.theme.OmegaCalendarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OmegaCalendarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Button(onClick = {
+                    val navigate = Intent(this@MainActivity, WeekView::class.java)
+                    startActivity(navigate)
+                }) {
+                    Text(text = "Go to week!", fontSize = 24.sp)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    OmegaCalendarTheme {
-        Greeting("Android")
     }
 }
