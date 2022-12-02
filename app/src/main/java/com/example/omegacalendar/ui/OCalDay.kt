e066f6eeca5200a0c3e64a2dff4d38382102e48d
RenGino <94270540+RenGino@users.noreply.github.com>
2022-12-02 10:24:52 -0800

    Added button functionality to day view events
    Options for events to be deleted and to be announce via discord

--- app/src/main/java/com/example/omegacalendar/ui/OCalDay.kt
+++ app/src/main/java/com/example/omegacalendar/ui/OCalDay.kt
@@ -1,28 +1,45 @@
 package com.example.omegacalendar.ui
 
+import android.content.Intent
+import android.text.Layout
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomEnd
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.omegacalendar.AddEventActivity
+import com.example.omegacalendar.MainActivity
 import com.example.omegacalendar.OCalScreen
 import com.example.omegacalendar.data.Event
 import com.example.omegacalendar.data.EventViewModel
+import com.example.omegacalendar.ui.theme.darkGrey
+import com.example.omegacalendar.ui.theme.tealBackground
+import com.example.omegacalendar.ui.theme.tealSecondary
 
 @Composable
 fun DailyScreen(
@@ -48,27 +65,34 @@ fun DailyScreen(
         11 -> "Dec"
         else -> "Invalid month"
     }
+
     Column(modifier = Modifier.wrapContentWidth()){
-        Text(
-
-            text = monthName + " " + day.toString() + ", " + year.toString(),
-
-            modifier = Modifier
-                .padding(16.dp),
-//                .fillMaxWidth()
-//                .weight(1 / 16f),
-            style = MaterialTheme.typography.h3
+        Row(){
+            Text(
+                text = monthName + " " + day.toString() + ", " + year.toString(),
+                modifier = Modifier
+                    .padding(16.dp),
+                style = MaterialTheme.typography.h3
             )
-        DayEventList(events)
+        }
+        DayEventList(events, viewModel)
+
     }
 }
 
 
 @Composable
-fun DayEventListItem(event: Event){
+fun DayEventListItem(event: Event, viewModel: EventViewModel){
+    val openDialog = remember { mutableStateOf(false) }
+
     Row() {
         Card() {
-            Row() {
+            Row(
+                modifier = Modifier
+                    .clickable(onClick = {
+                        openDialog.value=true
+                    })
+            ) {
                 Text(
                     text = event.desc,
                     modifier = Modifier
@@ -80,43 +104,66 @@ fun DayEventListItem(event: Event){
                     Text(
                         text = event.startHour.toString() + " - " + event.startMin.toString(),
                         modifier = Modifier
+                            .padding(top = 15.dp)
                             .weight(6 / 16f),
                         style = MaterialTheme.typography.h6
                 )
-
-//                Text(
-//                    text = event.startHour.toString() + ":" + event.startMin.toString(),
-//                    modifier = Modifier
-//                        .weight(6 / 16f),
-//                    style = MaterialTheme.typography.h6
-//                )
-//
-//                Text(
-//                    text = "-",
-//                    modifier = Modifier
-//                        .weight(1/16f),
-//                    style = MaterialTheme.typography.h6
-//                )
-//
-//                Text(
-//                    text = event.endHour.toString()+ ":" + event.endMin.toString(),
-//                    modifier = Modifier
-//                        .weight(2 / 16f),
-//                    style = MaterialTheme.typography.h6
-//                )
-
             }
         }
+    }
+
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                // Dismiss the dialog when the user clicks outside the dialog or on the back
+                // button. If you want to disable that functionality, simply use an empty
+                // onCloseRequest.
+                openDialog.value = false
+            },
+            title = {
+                Text(text = "Please select option")
+            },
+            buttons = {
+                Column(
+                    modifier = Modifier.padding(all = 8.dp),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {
+                            viewModel.deleteEvent(event)
+                            openDialog.value = false
+                        }
+                    ) {
+                        Text("Delete Event")
+                    }
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {
+                            // PUT CODE FOR DISCORD BOT HERE!!
+                            openDialog.value = false
+                        }
+                    ) {
+                        Text("Discord Announcement")
+                    }
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = { openDialog.value = false }
+                    ) {
+                        Text("Dismiss")
+                    }
+                }
+            }
+        )
     }
 }
 
 @Composable
-fun DayEventList(events: List<Event>){
+fun DayEventList(events: List<Event>, viewModel: EventViewModel){
     LazyColumn() {
         items(events) { event ->
-            DayEventListItem(event)
+            DayEventListItem(event, viewModel)
             Divider()
         }
     }
 }
-
