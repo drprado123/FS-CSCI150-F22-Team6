9c79aa290b473cdb75b8bc4eb74ca0c5ce72b9d4
drprado123 <103281716+drprado123@users.noreply.github.com>
2022-12-01 17:25:30 -0800

    Update MainActivity.kt

--- app/src/main/java/com/example/omegacalendar/MainActivity.kt
+++ app/src/main/java/com/example/omegacalendar/MainActivity.kt
@@ -40,4 +40,30 @@ fun DefaultPreview() {
     OmegaCalendarTheme {
         Greeting("Android")
     }
+<<<<<<< Updated upstream
+=======
+}
+
+fun networkrequest(info: String){ //this is the network request function it has to exist here in the main or it wont function
+    //val template = "Hello you have event {$Event.description}, at {$Event.time}"
+    val imageloc = "https://assets.stickpng.com/images/587185d57b7f6103e35c6cc7.png"
+    val requestModel = RequestModel("OmegaCalendarBot", info,imageloc)
+
+    val response = ServiceBuilder.buildService(ApiInterface::class.java)
+    response.sendReq(requestModel).enqueue(
+        object : Callback<ResponseModel> {
+            override fun onResponse(
+                call: Call<ResponseModel>,
+                response: Response<ResponseModel>
+            ) {
+                return
+            }
+
+            override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
+                return
+            }
+
+        }
+    )
+>>>>>>> Stashed changes
 }
