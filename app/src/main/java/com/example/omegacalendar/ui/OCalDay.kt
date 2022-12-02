3ff459dd8b6194fa425b2a98c6733c6299ee8808
FlamingEmbr <90358980+FlamingEmbr@users.noreply.github.com>
2022-12-01 17:59:18 -0800

    something

--- app/src/main/java/com/example/omegacalendar/ui/OCalDay.kt
+++ app/src/main/java/com/example/omegacalendar/ui/OCalDay.kt
@@ -3,7 +3,6 @@ package com.example.omegacalendar.ui
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-//import androidx.compose.foundation.layout.BoxScopeInstance.align
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
