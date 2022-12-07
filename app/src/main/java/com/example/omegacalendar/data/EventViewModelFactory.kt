d8c5f7514ae48f4f4aaeeb60d049fc68a8f5df12
kiniwaka <112026761+kiniwaka@users.noreply.github.com>
2022-12-06 17:20:40 -0800

    comment

--- app/src/main/java/com/example/omegacalendar/data/EventViewModelFactory.kt
+++ app/src/main/java/com/example/omegacalendar/data/EventViewModelFactory.kt
@@ -3,7 +3,7 @@ package com.example.omegacalendar.data
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import java.lang.IllegalArgumentException
-
+// type casting a viewmodel to turn it into the one we used in the app, takes DAO as input to be used in the viewmodel
 class EventViewModelFactory(
     private val dao: EventDao
 ):ViewModelProvider.Factory{
