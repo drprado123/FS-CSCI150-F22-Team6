93a769191c3e111bf32f1831bf5259d6d554ce0b
kiniwaka <112026761+kiniwaka@users.noreply.github.com>
2022-12-06 17:03:54 -0800

    comment

--- app/src/main/java/com/example/omegacalendar/data/AppDatabase.kt
+++ app/src/main/java/com/example/omegacalendar/data/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-
+//create the database using room so we can access Events and store them database on first launch of the app maintained and accessed in all subsequent startups
 @Database(entities = [Event::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase(){
 
