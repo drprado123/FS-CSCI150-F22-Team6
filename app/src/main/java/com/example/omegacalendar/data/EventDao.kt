5d54e48ef13a740d05ce1004093b1155725d4f53
kiniwaka <112026761+kiniwaka@users.noreply.github.com>
2022-12-06 17:08:52 -0800

    Comments

--- app/src/main/java/com/example/omegacalendar/data/EventDao.kt
+++ app/src/main/java/com/example/omegacalendar/data/EventDao.kt
@@ -8,9 +8,12 @@ import androidx.room.*
  *  lets us interface with the sqlite db easily through the dao to query and \
  *  interface with entities that are added or retrieved from db
  */
+// Use DAO to interface with app database and interact with DAO for queries and persist changes back to database
+// DAO converts database results into event objects that we can interact with in the app. 
 @Dao
 interface EventDao {
     // functions implemented in viewmodel for easy access and reuse, we utilize coroutines
+    // insert, delete, and update are queries that are predefined by ROOMDB
     @Insert
     suspend fun insertEvent(event: Event)
 
@@ -21,6 +24,7 @@ interface EventDao {
     suspend fun deleteEvent(event: Event)
 
     //add queries here
+    // @Query is an input to ROOMDB that takes a string in SQL format and binds that parsed sql call to the kotlin function so when the kotlin function is called sql is executed
     @Query("SELECT * FROM events_table ORDER BY event_year")
     fun getEvents() : LiveData<List<Event>>
 
